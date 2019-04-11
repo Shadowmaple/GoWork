c9de511cdff95929170f17f8d5c4b7d053ac390f
Shadowmaple <1142319190@qq.com>
2019-04-11 22:36:17 +0800

    get from net

---
+++ pat1002+.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var str []byte
+	var sum int
+	var number = [...]string{"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"}
+
+	fmt.Scanf("%s", &str)
+//	fmt.Println(str)
+	strlen := len(str)
+	for i:=0; i<strlen; i++ {
+		sum += int(str[i]) - int('0')
+	}
+
+	sumArray := []byte(strconv.Itoa(sum))
+	for j:=0; j<len(sumArray); j++ {
+		if j+1 == len(sumArray) {
+			fmt.Printf("%s\n", number[int(sumArray[j]) - int('0')])
+		} else {
+			fmt.Printf("%s ", number[int(sumArray[j]) - int('0')])
+		}
+	}
+}
