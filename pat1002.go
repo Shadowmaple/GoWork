3e40d53ea88a512effccc4ae2423e431940ffb55
Shadowmaple <1142319190@qq.com>
2019-04-11 22:26:17 +0800

    practice

---
+++ pat1002.go
@@ -0,0 +1,24 @@
+package main
+import ("fmt"; "strconv")
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	var ch = [10]string{"ling","yi","er","san","si","wu","liu","qi","ba","jiu"}
+	var sum = 0
+
+	for n > 0 {
+		sum += n % 10
+		n /= 10
+	}
+	fmt.Println(sum)
+	l := len(strconv.Itoa(sum))
+	for i, s := range strconv.Itoa(sum) {
+		m, _ := strconv.Atoi(string(s))
+		fmt.Printf("%s", ch[m])
+		if i != l-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("\n")
+}
