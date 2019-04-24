5880ec2281fd69440d0d44f8fb376b094f759dad
Shadowmaple <1142319190@qq.com>
2019-04-24 22:59:17 +0800

    modify

--- leetcode14.go
+++ leetcode14.go
@@ -1,11 +1,18 @@
 package main
 import "fmt"
 
-func longestCommonPrefix(strs []string) string {	
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
 	length := len(strs[0])
 	for index := 0; index < length; index++ {
 		prefix := strs[0][:index+1]
 		for _, ch := range strs[1:] {
+			if len(ch) < len(prefix) {
+				return strs[0][:index]
+			}
 			if prefix != ch[:index+1] {
 				return strs[0][:index]
 			}
@@ -15,6 +22,7 @@ func longestCommonPrefix(strs []string) string {
 }
 
 func main() {
-	s := []string{"flower", "flow", "flight"}
+	//s := []string{"flower", "flow", "flight"}
+	s := []string{}
 	fmt.Println(longestCommonPrefix(s))
 }
