60fe5ba27445a0bbdac834cb3548c40c9b7d9dce
Shadowmaple <1142319190@qq.com>
2019-04-25 22:23:52 +0800

    modify

--- leetcode/15_threeSum+.go
+++ leetcode/15_threeSum+.go
@@ -4,29 +4,38 @@ import (
 	"sort"
 )
 
+//执行用时 : 1472 ms, 在3Sum的Go提交中击败了77.63% 的用户
+//内存消耗 : 82.4 MB, 在3Sum的Go提交中击败了45.48% 的用户
 func threeSum(nums []int) [][]int {
-	sort.Ints(nums)
 	var data [][]int
+	sort.Ints(nums)
+	length := len(nums)
 
-	for aPositoin, a := range nums {
-		for bPosition := len(nums)-1; bPosition >= 0; bPosition-- {
-			if bPosition == aPositoin {
+	for aIndex, a := range nums {
+		if a > 0 {
+			break
+		}
+		if aIndex > 0 && nums[aIndex] == nums[aIndex-1] {
+			continue
+		}
+		bIndex := aIndex + 1
+		cIndex := length - 1
+		for ; bIndex < cIndex; {
+			if a + nums[bIndex] == -nums[cIndex] {
+				data = append(data, []int{a, nums[bIndex], nums[cIndex]})
+				for bIndex < cIndex && nums[bIndex] == nums[bIndex+1] {
+					bIndex++
+				}
+				for bIndex < cIndex && nums[cIndex] == nums[cIndex-1] {
+					cIndex--
+				}
+				bIndex++; cIndex--
 				continue
 			}
-			b := nums[bPosition]
-			c := -(a + b)
-			flag := false
-			for i, n := range nums {
-				if i == aPositoin || i == bPosition {
-					continue
-				}
-				if n == c {
-					flag = true
-					break
-				}
-			}
-			if flag {
-				data = append(data, []int{a, b, c})
+			if a + nums[bIndex] < -nums[cIndex] {
+				bIndex++
+			} else {
+				cIndex--
 			}
 		}
 	}
