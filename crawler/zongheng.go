e9aac447a1689bc59e465aacaa82e568c468cae7
Shadowmaple <1142319190@qq.com>
2019-05-31 20:03:57 +0800

    modify

---
+++ crawler/zongheng.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	var lock sync.Mutex
+	var wait sync.WaitGroup
+
+	var keyword string
+	keyword = "火影"
+	//fmt.Scanln(&keyword)
+
+	file, _ := os.OpenFile("zongheng.txt", os.O_WRONLY | os.O_CREATE, 0666)
+	num := 1
+
+	for i := 1; i <= 10; i++ {
+		requestUrl := "http://search.zongheng.com/s?keyword=" + keyword + "&pageNo=" + strconv.Itoa(i) + "&sort="
+		wait.Add(1)
+		go func () {
+			defer wait.Done()
+
+			rp, _ := http.Get(requestUrl)
+			body, _ := ioutil.ReadAll(rp.Body)
+			content := string(body)
+			defer rp.Body.Close()
+
+			dom, _ := goquery.NewDocumentFromReader(strings.NewReader(content))
+			dom.Find(".search-tab").Each(func(i int, selection *goquery.Selection){
+				//fmt.Println(selection.Text())
+				wait.Add(1)
+				go func () {
+					defer wait.Done()
+					selection.Find(".tit").Each(func(i int, title *goquery.Selection) {
+						lock.Lock()
+						defer lock.Unlock()
+						//fmt.Println(title.Text())
+						fmt.Fprintf(file, "%3d   ", num)
+						fmt.Fprintln(file, title.Text())
+						num++
+					})
+					/*
+					selection.Find(".bookinfo").Each(func(i int, author *goquery.Selection) {
+						//fmt.Println(author.Text())
+						fmt.Fprintln(file, author.Text())
+					})
+					 */
+				}()
+				})
+		}()
+
+	}
+	wait.Wait()
+	println("elapsed:", time.Since(t))
+}
