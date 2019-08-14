39b977f5ec01210af8148e91372db382a9a4751a
Shadowmaple <1142319190@qq.com>
2019-08-14 17:38:36 +0800

    sort the import-packages

--- mongo/main.go
+++ mongo/main.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"fmt"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
