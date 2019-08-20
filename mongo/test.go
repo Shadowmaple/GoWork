8068657397fae0a7cfa4016ef402fcc8a3182b0a
Shadowmaple <1142319190@qq.com>
2019-08-20 20:26:06 +0800

    out

--- mongo/test.go
+++ mongo/test.go
@@ -1,1 +1,30 @@
 package main
+
+//import (
+//	"context"
+//	"log"
+//	"time"
+//
+//	"go.mongodb.org/mongo-driver/bson"
+//	"go.mongodb.org/mongo-driver/mongo"
+//	"go.mongodb.org/mongo-driver/mongo/options"
+//)
+//
+//func Test() {
+//	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//
+//	//ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+//	//if err := client.Connect(ctx); err != nil {
+//	//	log.Fatal(err)
+//	//}
+//	//
+//	//col := client.Database("table").Collection("test")
+//	//
+//	//if _, err := col.InsertOne(ctx, bson.D{{"name", "Shadow"}}); err != nil {
+//	//	log.Fatal(err)
+//	//}
+//
+//}
