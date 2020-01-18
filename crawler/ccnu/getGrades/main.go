f6ab7b91bf632eb2e7e55d0f4c6e4579ea9f26c4
Shadowmaple <1142319190@qq.com>
2020-01-18 21:29:33 +0800

    modify

---
+++ crawler/ccnu/getGrades/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"net/http"
+	"io/ioutil"
+)
+
+func main() {
+
+//	url := "http://xk.ccnu.edu.cn/cjcx/cjcx_cxCjxq.html?time=1579246531974&gnmkdm=N305005"
+	url := "http://xk.ccnu.edu.cn/cjcx/cjcx_cxCjxq.html?time=" + strconv.Itoa(int(time.Now().UnixNano())) + "&gnmkdm=N305005"
+	method := "POST"
+
+	payload := strings.NewReader("jxb_id=8AF0256B86F10FA8E053CADCDC0A2E88&xnm=2019&xqm=3&kcmc=%E6%A6%82%E7%8E%87%E7%BB%9F%E8%AE%A1A")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Accept", "text/html, */*; q=0.01")
+	req.Header.Add("Origin", "http://xk.ccnu.edu.cn")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+	req.Header.Add("Cookie", "JSESSIONID=FA254977BA95A1E5BAC56C0F18BF0476")
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	fmt.Println(string(body))
+}
