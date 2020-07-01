ea115e3bea97ec098f1e2cf1d7f30ffc4c132471
Shadowmaple <1142319190@qq.com>
2020-07-01 18:55:23 +0800

    :fire: modify token parsing function

--- go.mod
+++ go.mod
@@ -1,25 +1,19 @@
-module github.com/Shadowmaple/Go
+module github.com/Shadowmaple/GoWork
 
 go 1.12
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
-	github.com/apache/rocketmq-client-go v1.2.4 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.5.0
 	github.com/go-redis/redis/v7 v7.2.0
 	github.com/howeyc/gopass v0.0.0-20190910152052-7cb4b85ec19c
-	github.com/imrenagi/go-oauth2-mysql v1.1.0 // indirect
 	github.com/jinzhu/gorm v1.9.14
 	github.com/lexkong/log v0.0.0-20180607165131-972f9cd951fc
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/viper v1.7.0
 	github.com/tidwall/gjson v1.6.0
 	go.mongodb.org/mongo-driver v1.3.1
-	golang.org/x/crypto v0.0.0-20191206172530-e9b2fee46413 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	google.golang.org/grpc v1.28.0 // indirect
-	gopkg.in/go-oauth2/mysql.v3 v3.0.4 // indirect
-	gopkg.in/gorp.v2 v2.2.0 // indirect
 	gopkg.in/oauth2.v4 v4.0.0
 )
--- oauth/token/main.go
+++ oauth/token/main.go
@@ -7,11 +7,17 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+var jwtKey = "Rtg8BPKNEf2mB4mgvKONGPZZQSaJWNLijxR42qRgq0iBb5"
+
 func main() {
-	token := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ0ZXN0IiwiZXhwIjoxNTkyNjUzMzI0LCJzdWIiOiIxMjMifQ.QAYq3BgTIgBHkDYqdYkn5RA3sNZJ_03AzkNbE_uYtJiuwlwEiEF1xnpUZZbpR9lrzvrE2YMKxPDT9wWyEyrmyQ"
+	token := "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhODUwZGE2NC0zMTBlLTQxNmYtYTZjMy1hZDlhN2FkN2ViMjUiLCJleHAiOjE1OTM1OTc1ODIsInN1YiI6IjI1NCJ9.Bnqp9hTWLudtjsGP2eZmWSWiuwKBsdkrAuGxKJxdgyFOgKuPjwRFKWP3gXR2_aFb-ezNB0o9sxYnuzwnDm8A7Q"
 
 	s, err := ResolveToken(token)
 	if err != nil {
+		if IsErrTokenExpired(err) {
+			fmt.Println("token is expired.")
+			return
+		}
 		panic(err)
 	}
 	fmt.Println(s)
@@ -22,20 +28,24 @@ func ResolveToken(Token string) (string, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
-
-		return []byte("oauth"), nil
+		return []byte(jwtKey), nil
 	})
+
 	if err != nil {
 		return "", err
 	}
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		fmt.Println(claims)
-		if sub, ok := claims["sub"]; !ok {
+		sub, ok := claims["sub"]
+		if !ok {
 			return "", errors.New("Token not include `cap` field.")
-		} else {
-			return sub.(string), nil
 		}
+		return sub.(string), nil
 	}
 	return "", errors.New("Unknown error.")
+}
+
+func IsErrTokenExpired(err error) bool {
+	return err.Error() == "Token is expired"
 }
