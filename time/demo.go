ddf270e92ed66e9659ef2eebe054387443ef2323
Shadowmaple <1142319190@qq.com>
2020-07-12 15:29:49 +0800

    update time demo

--- time/demo.go
+++ time/demo.go
@@ -8,11 +8,19 @@ import (
 func main() {
 	t := GetCurrentTime()
 	fmt.Println(t)
+
+	TimeFormat()
 }
 
 func GetCurrentTime() *time.Time {
-//	var loc, _ = time.LoadLocation("Asia/Shanghai")
-//	loc := time.FixedZone("CST", 7*3600)
+	//	var loc, _ = time.LoadLocation("Asia/Shanghai")
+	//	loc := time.FixedZone("CST", 7*3600)
 	t := time.Now().UTC().Add(8 * time.Hour)
 	return &t
+}
+
+func TimeFormat() {
+	now := time.Now()
+	s := now.Format("2006/01/02 15:04:05")
+	fmt.Println(s)
 }
