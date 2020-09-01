200326235f50dd8ebc1fb7c6c529c2b087fb1229
Shadowmaple <1142319190@qq.com>
2020-09-01 13:39:57 +0800

    new

---
+++ hash/auth/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"hash/crc32"
+)
+
+func main() {
+	hash := Hash("frontend")
+	fmt.Println(hash)
+}
+
+func Hash(password string) uint64 {
+	v := int64(crc32.ChecksumIEEE([]byte(password)))
+	if v >= 0 {
+		return uint64(v)
+	}
+	if -v >= 0 {
+		return uint64(-v)
+	}
+	// v == MinInt
+	return 0
+}
