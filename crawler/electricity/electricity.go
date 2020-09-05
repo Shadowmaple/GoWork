4b2a575a08e3f966db6a90566249357577b1f1a2
Shadowmaple <1142319190@qq.com>
2020-09-05 21:13:51 +0800

    update

--- crawler/electricity/electricity.go
+++ crawler/electricity/electricity.go
@@ -1,49 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-)
-
 func main() {
-	GetElectricCharge("")
-}
 
-func GetElectricCharge(RoomId string) {
-	// var charge model.ElectricCharge
-
-	// 第二个URL的 query要求以“2020/5/30”格式输入昨天的日期
-	// now := time.Now().AddDate(0, 0, -1)
-	// query := now.Format("2006/01/02")
-	firUrl := "http://jnb.ccnu.edu.cn/icbs/PurchaseWebService.asmx/getReserveHKAM?AmMeter_ID=" + RoomId
-	// secUrl := "http://jnb.ccnu.edu.cn/icbs/PurchaseWebService.asmx/getMeterDayValue?AmMeter_ID=" + RoomId + "&startDate=" + query + "&endDate=" + query
-
-	resp, err := http.Get(firUrl)
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(body))
-
-	// xml.Unmarshal(body, &charge)
-	// res, err := http.Get(secUrl)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// defer res.Body.Close()
-	// newBody, err := ioutil.ReadAll(res.Body)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// charge.Id = RoomId
-	// xml.Unmarshal(newBody, &charge)
-	// return charge, nil
 }
