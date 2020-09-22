3311a7b15592eb6cb7fa735f24d80124e39a0af5
Shadowmaple <1142319190@qq.com>
2020-09-22 10:10:30 +0800

    update

--- regexp/parseGrade/demo.go
+++ regexp/parseGrade/demo.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"regexp"
-	"strconv"
 )
 
 func main() {
@@ -22,17 +21,16 @@ func main() {
 					<tr>
 						<td valign="middle">【 期末 】</td>
 						<td valign="middle">60%&nbsp;</td>
-						<td valign="middle">77.3&nbsp;</td>
+						<td valign="middle">缓考&nbsp;</td>
 					</tr>
 
 					<tr>
 						<td valign="middle">【 总评 】</td>
-						<td valign="middle">&nbsp;</td>
+						<td valign="middle">0%&nbsp;</td>
 						<td valign="middle">70.8&nbsp;</td>
 					</tr></html>`
-	//	rg, err := regexp.Compile(`<td valign="middle">[^%]*&nbsp;</td>`)
-	rg, err := regexp.Compile(`<td valign="middle">([0-9|\\.]*)&nbsp;</td>`)
-	// rg, err := regexp.Compile(`<td valign="middle">(.*)</td>`)
+	rg, err := regexp.Compile(`<td valign="middle">(.*)&nbsp;</td>`)
+	// rg, err := regexp.Compile(`<td valign="middle">([\d|\\.|%]*)&nbsp;</td>`)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -48,14 +46,6 @@ func main() {
 	// 	fmt.Println(s[v[0]:v[1]])
 	// }
 
-	x := rg.FindAllStringSubmatch(s, 2)
+	x := rg.FindAllStringSubmatch(s, -1)
 	fmt.Println(x)
-
-	if len(x) < 2 {
-		return
-	}
-
-	u, err := strconv.ParseFloat(x[0][1], 32)
-	f, err := strconv.ParseFloat(x[1][1], 32)
-	fmt.Println(float32(u), " ", float32(f))
 }
