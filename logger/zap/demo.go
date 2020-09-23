507385373eb3ef8e7558fa1fd9530d2bdc381b25
Shadowmaple <1142319190@qq.com>
2020-09-23 15:48:56 +0800

    update

--- logger/zap/demo.go
+++ logger/zap/demo.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	logger.Error("failed to fetch URL",
+		// Structured context as strongly typed Field values.
+		zap.String("url", "localhost:88"),
+		zap.Int("attempt", 3),
+		zap.Duration("backoff", time.Second),
+	)
+}
